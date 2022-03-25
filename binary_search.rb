d901bcbf94ecc39904b4c9a23740372f5d535425
Pedro Fernandes <pjf_fernandes@yahoo.com.br>
2022-03-25 17:32:29 -0300

    bubble sort

--- binary_search.rb
+++ binary_search.rb
@@ -17,3 +17,21 @@ def binary_search(array, n)
 end
 
 binary_search([1, 2, 3, 4, 10, 2, 5, 21, 23], 2)
+#####################################
+def bubble_sort(array)
+  array = array
+  for j in (0..array.size - 2)
+    for i in (0..array.size - 1)
+      if array[i+1].nil? == false
+        if array[i] > array[i+1]
+          aux = array[i]
+          array[i] = array[i+1]
+          array[i+1] = aux
+        end
+      end
+    end
+  end
+  array
+end
+
+bubble_sort([1, 2, 3, 4, 10, 2, 5, 21, 23])
